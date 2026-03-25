c66823b5915759cc0cd984a22d2140d5cfc0949f
alex2772 <alex2772sc@gmail.com>
2026-03-25 04:18:06 +0300

    docker compose

---
+++ ollama_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Start Ollama in the background.
+#OLLAMA_CONTEXT_LENGTH=40000 OLLAMA_DEBUG=1 /bin/ollama serve &
+OLLAMA_CONTEXT_LENGTH=128000 OLLAMA_DEBUG=1 /bin/ollama serve &
+# Record Process ID.
+pid=$!
+
+# Pause for Ollama to start.
+sleep 5
+
+echo "🔴 Retrieve LLAMA3 model..."
+#ollama pull gemma3:27b
+#ollama pull gpt-oss:20b
+echo "🟢 Done!"
+
+# Wait for Ollama process to finish.
+wait $pid
